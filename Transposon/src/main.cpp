d5bd2e8f5b5e79cd72fe10a95f355fca3f72e3ae
Kamil S Jaroň <376090@mail.muni.cz>
2017-12-19 11:24:55 +0100

    makefile and main manage to integrate git into log

--- Transposon/src/main.cpp
+++ Transposon/src/main.cpp
@@ -6,81 +6,107 @@
 
 #include "random.h"
 #include "time.h"
+#include "gitversion.h"
 
 using namespace std;
 
-int main ()
-{
-  cerr << endl;
-  const char *input_file = "input.txt";
-  const char *detailed_out = "detailed.txt";
-  const char *summary_out = "summary.txt";
+int main(int argc, char **argv){
 
-  for (int run=1; run<=10; run++)
-  {
+    string input_file = "input.txt";
 
+    if(argc > 1){
+        if(strcmp( argv[1], "--help") == 0 or strcmp( argv[1], "-h") == 0){
+            cout << "most of parameters are read form an input file (specified by argument or file input.txt by default)" << endl;
+            cout << "details are in readme file" << endl;
+            cout << "\t Transposon [--version] [--help] [input_file.txt]" << endl;
 
-    cerr << "Run : " << run << endl;
-    std::ifstream fin(input_file);
-        if (! fin.is_open())
-          { cerr << "Error opening file"; exit (1); }
-
-        // Initialize population size & whether to generate new population or load from file
-        int N;
-
-        /// fromFile=true is caling a code that reads stuff from a file that is missing
-        /// code was commented and warning message was added
-        bool fromFile = false;
-        char tempChar[100];
-        fin.getline(tempChar,100);
-        N=strtol(tempChar,0,10);
-    fin.close();
-
-    Population * pop = new Population(N);
-    Population * temp;
-
-    int size = pop->GetPopSize();
-    bool clonal = Genome::clonal;
-
-    pop->Initialize(clonal, fromFile);
-    pop->PrintParameters(detailed_out);
-
-    if (run==1) pop->PrintParameters(summary_out);
-    pop->SummaryStatistics(detailed_out, 0);
-
-    for (int gen = 1; gen <= 990; gen++) {
-        // cerr << "Running generation " << gen << "." << endl;
-
-        if (pop->GetPopulationTECount() == 0 or ((double)pop->GetPopulationTECount()/(double)size) > 150.0)
-        {
-          // cerr << "No TEs at generation [" << gen << "]." << endl << endl;
-          // cerr << "Population extinction at generation [" << gen << "]." << endl << endl;
-          pop->SummaryStatistics(summary_out, gen);
-          pop->SummaryStatistics(detailed_out, gen);
-          break;
+            return 0;
+        }
+        if(strcmp( argv[1], "--version") == 0 or strcmp( argv[1], "-v") == 0){
+            cout << "Transposon v1.? fork" << endl;
+            cout << "\thttps://github.com/KamilSJaron/yeast_TE_load_prediction/tree/master/Transposon" << endl;
+            /* start of dev vesion */
+            cout << "\tcommit: " << GITVERSION << endl;
+            /* end */
+            return 0;
         }
 
-        // cerr << "Reproducing " << endl;
-        // REPRODUCTION
-        temp = pop->AsexualReproduction();
-        delete pop;
-        pop = temp;
+        input_file = argv[1];
 
-        // cerr << "Transposing " << endl;
-        // TRANSPOSITION & LOSS
-        pop->TranspositionAndLoss();
-
-        /// printing results after transposition
-        cerr << ".";
-        if (gen % 90 == 0) {
-          cerr << endl;
-          pop->SummaryStatistics(detailed_out, gen);
-        }
     }
 
-    delete pop;
+    cerr << "Running Transposon" << endl;
+    /* start of dev vesion */
+    cerr << "Commit: " << GITVERSION << endl;
+    /* end */
+    cerr << "Loading ... " << input_file << endl;
 
-  }
+    /* constant arrays */
+    const char *detailed_out = "detailed.txt";
+    const char *summary_out = "summary.txt";
+
+    for (int run=1; run<=10; run++) {
+        cerr << "Run : " << run << endl;
+        std::ifstream fin(input_file);
+            if (! fin.is_open())
+              { cerr << "Error opening file : " << input_file << endl; exit (1); }
+
+            // Initialize population size & whether to generate new population or load from file
+            int N;
+
+            /// fromFile=true is caling a code that reads stuff from a file that is missing
+            /// code was commented and warning message was added
+            bool fromFile = false;
+            char tempChar[100];
+            fin.getline(tempChar,100);
+            N=strtol(tempChar,0,10);
+        fin.close();
+
+        Population * pop = new Population(N);
+        Population * temp;
+
+        int size = pop->GetPopSize();
+        bool clonal = Genome::clonal;
+
+        pop->Initialize(clonal, fromFile);
+        pop->PrintParameters(detailed_out);
+
+        if (run==1) pop->PrintParameters(summary_out);
+        pop->SummaryStatistics(detailed_out, 0);
+
+        for (int gen = 1; gen <= 990; gen++) {
+            // cerr << "Running generation " << gen << "." << endl;
+
+            if (pop->GetPopulationTECount() == 0 or ((double)pop->GetPopulationTECount()/(double)size) > 150.0)
+            {
+              // cerr << "No TEs at generation [" << gen << "]." << endl << endl;
+              // cerr << "Population extinction at generation [" << gen << "]." << endl << endl;
+              pop->SummaryStatistics(summary_out, gen);
+              pop->SummaryStatistics(detailed_out, gen);
+              break;
+            }
+
+            // cerr << "Reproducing " << endl;
+            // REPRODUCTION
+            temp = pop->AsexualReproduction();
+            delete pop;
+            pop = temp;
+
+            // cerr << "Transposing " << endl;
+            // TRANSPOSITION & LOSS
+            pop->TranspositionAndLoss();
+
+            /// printing results after transposition
+            cerr << ".";
+            if (gen % 90 == 0) {
+              cerr << endl;
+              pop->SummaryStatistics(detailed_out, gen);
+            }
+        }
+
+        delete pop;
+
+    }
 
   return 0;
 }
