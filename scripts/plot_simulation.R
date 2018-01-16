8acafe89ea30ecd14d2170f63d3d8dba26572993
Kamil S Jaroň <376090@mail.muni.cz>
2018-01-17 00:05:56 +0100

    presentation

--- scripts/plot_simulation.R
+++ scripts/plot_simulation.R
@@ -5,10 +5,16 @@ args <- commandArgs(trailingOnly = TRUE)
 
 source('scripts/read_simulation.R')
 
-# 'sims/001_basic_diploid_ch2/detailed.txt'
+# 'sims/003_basic_sex/detailed.txt'
 sim <- read_simulation( args[1] )
 
 TEs <- unlist(sapply(sim, function(x){ x$n }))
+
+if( grepl('asex', args[1]) ){
+    col <- "#92C5DECD"
+} else {
+    col <- "#D6604DED"
+}
 
 # 'figures/sim_2ch_literature_pars.png'
 png( args[2] )
@@ -18,6 +24,6 @@ plot(NULL,
     xlab = 'Generation', ylab = 'number of TEs',
     main = 'simulated TEs')
 
-all_plots <- sapply(sim, function(x){ lines(x$n ~ x$GEN) })
+all_plots <- sapply(sim, function(x){ lines(x$n ~ x$GEN, col = col, lwd = 1.4) })
 
 dev.off()
