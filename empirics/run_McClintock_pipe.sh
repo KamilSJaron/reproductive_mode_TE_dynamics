4e25a9bb0dc28b3f111db2737e96aa1b8cc1f66a
Jens Bast <jbast@dee-serv04.vital-it.ch>
2018-10-10 12:42:20 +0200

    scripts update

--- empirics/run_McClintock_pipe.sh
+++ empirics/run_McClintock_pipe.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#decompress fq.gz and run McClintock pipeline on the yeast data
+#run McClintock pipeline on the yeast data
  
 clear
 while read line; do
   read -a twoSp <<< $line
   printf "\n****processing ${twoSp[0]}*****\n"
   SP1=${twoSp[0]}
- 
-#decompress reads
-  #printf "**decompressing $SP1**\n\n"
-  #gunzip /scratch/beegfs/monthly/jbast/yeast/processed_reads_trim/$SP1''_1_paired.fq.gz
-  #gunzip /scratch/beegfs/monthly/jbast/yeast/processed_reads_trim/$SP1''_2_paired.fq.gz
  
 #run pipe
   printf "**running $SP1**\n\n"
